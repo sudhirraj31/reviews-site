da9d4958c17a23246a0718f9e044ee6b99fe2bc9
Sudhir <sudhirraj31@gmail.com>
2022-06-30 17:09:37 +0530

    .env file issue still exists directly updated api

--- src/reviewer/components/Review.js
+++ src/reviewer/components/Review.js
@@ -10,7 +10,6 @@ import colors from "../../config/colors";
 
 export default function Review({ review }) {
   let currentLikes = review.like;
-  console.log(currentLikes);
   const [like, setLike] = useState(currentLikes);
   const [likeIconColor, setLikeIconColor] = useState(colors.lightgrey);
   const handleLike = (e) => {
--- tasks.md
+++ tasks.md
@@ -1,4 +1,3 @@
 2. reviwer can only see own reviews on reviwer dashboard
 3. one like per user
-4. like counts display on particular review page
-5. file uploader in creating review
+4. file uploader in creating review
